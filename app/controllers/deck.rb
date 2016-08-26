b25d2d7930b16f3f14b6ab0cbf4d8c9021808375
Moin Uddin  & Richie Ito & Terence Stone <pair+legitcoder.ricito4.terencestone@devbootcamp.com>
2016-08-26 14:12:49 -0400

    worked on controlling cards

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,1 +1,15 @@
-deck.rb
+get '/decks/:id/show' do
+  @deck= Deck.find(params[:id])
+  erb :'/decks/show'
+end
+
+get '/deck/:id/play' do
+  @deck= Deck.find(params[:id])
+  @guesses= Guess.where
+  erb :"/decks/ask"
+end
+
+
+post '/decks/:id/check' do
+
+end
