93daac7fd4f0fc772dc14d976a963f87c03751b3
Moin Uddin <email": null>
2016-08-26 12:38:42 -0400

    Added user authentication login and logout features

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,15 +1,20 @@
-get '/' do
-  erb :'index'
+get '/login' do
+  erb :'users/login'
 end
 
-post '/' do
+post '/login' do
   @username = params[:user][:username]
   @user  = User.find_by(username: @username)
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
-    erb :index
+    redirect '/'
   else
     @errors = ['Invalid username/password']
     erb :'/users/login'
   end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,1 +1,13 @@
-user.rb
+get '/user/new' do
+  erb :'users/new'
+end
+
+post '/user' do
+  user = User.new(params[:user])
+  if user.save
+    redirect '/'
+  else
+    @errors = user.errors.full_messages
+    erb :'users/new'
+  end
+end
