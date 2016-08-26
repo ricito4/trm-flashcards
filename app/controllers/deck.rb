3a2bf07a9c5b344364b52564f26ced0d957487c7
Moin Uddin  & Richie Ito & Terence Stone <pair+legitcoder.ricito4.terencestone@devbootcamp.com>
2016-08-26 17:49:00 -0400

    got it to prent the correct first try guesses

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -31,9 +31,12 @@ get '/decks/:round_id/play' do
     end
   end
 
-
-  @card= included_cards.shuffle[0]
-  erb :"/decks/play"
+    if included_cards.empty?
+      redirect "/decks/#{@round.id}/finish"
+    else
+      @card= included_cards.shuffle[0]
+      erb :"/decks/play"
+    end
 end
 
 
@@ -45,7 +48,32 @@ post '/decks/:round_id/check' do
     redirect "/decks/#{params[:round_id]}/play"
   else
     Guess.create(user_id: current_user.id, card_id: card.id, round_id: params[:round_id], correct: false)
-    erb :"/decks/#{params[:round_id]}/play"
+    redirect "/decks/#{params[:round_id]}/play"
   end
 end
 
+get "/decks/:round_id/finish" do
+  @round= Round.find(params[:round_id])
+  @deck= Deck.find_by(id: @round.deck_id)
+  @guesses= Guess.where(round_id: @round.id)
+
+  # @correct_guesses = []
+  @true_guesses = []
+  false_guesses = []
+
+  @guesses.each do |guess|
+    if guess.correct
+      @true_guesses << guess
+    else
+      false_guesses << guess
+    end
+  end
+
+  false_guesses.each do |f_guess|
+    @true_guesses.delete_if do |t_guess|
+      t_guess.card_id == f_guess.card_id
+    end
+  end
+
+  erb :'/decks/finish'
+end
